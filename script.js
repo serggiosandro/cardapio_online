d8240ad109ff5b23688d465e89153ca21a1842e9
serggiosandro <soarescarvalho8103@gmail.com>
2025-06-03 14:37:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestauranteOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 10 && hora < 22; 
+    return hora >= 18 && hora < 22; 
     //true restaurante esta aberto 
 
 }
